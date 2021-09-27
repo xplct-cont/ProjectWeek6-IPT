d087e57bd74f955c78c2a7f7dde948137fdacfa2
KennSecusana <kennsecusana@gmail.com>
2021-09-28 07:10:13 +0800

    week6

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -13,7 +13,7 @@ class Course extends Model
     }
 
     public function trainees(){
-        return $this->hasMany('App\MOdels\Trainee');
+        return $this->hasMany('App\M0dels\Trainee');
     }
     //use HasFactory;
 }
